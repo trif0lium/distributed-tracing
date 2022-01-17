4156b54828ba1ee9fa11675fd9e491e420bdfd28
trif0lium <gh@fleeingsunlight.dev>
2022-01-17 17:56:41 +0700

    feat(character/app): GraphQLFederationModule

--- mhy/apps/character/src/app/app.module.ts
+++ mhy/apps/character/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { CharacterModule } from '@mhy/character/feature-character'
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLFederationModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLFederationModule.forRoot({
+      autoSchemaFile: true
+    }),
+    CharacterModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
