9a4b16a4016336a2827b5f9245f8806005ec869b
trif0lium <gh@fleeingsunlight.dev>
2022-01-17 17:53:57 +0700

    feat(character/feature-character): add `characters` query

--- mhy/libs/character/feature-character/src/character/character.resolver.ts
+++ mhy/libs/character/feature-character/src/character/character.resolver.ts
@@ -1,4 +1,9 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query } from '@nestjs/graphql';
 
 @Resolver()
-export class CharacterResolver {}
+export class CharacterResolver {
+  @Query(() => [String])
+  characters(): string[] {
+    return []
+  }
+}
