c772253b685ca598e5301cc20d65e4185979c3cb
trif0lium <gh@fleeingsunlight.dev>
2022-01-17 18:17:23 +0700

    feat(character/feature-character): service.getCharacters()

--- mhy/libs/character/feature-character/src/character/character.resolver.ts
+++ mhy/libs/character/feature-character/src/character/character.resolver.ts
@@ -1,9 +1,12 @@
 import { Resolver, Query } from '@nestjs/graphql';
+import { CharacterService } from './character.service';
 
 @Resolver()
 export class CharacterResolver {
+  constructor(private readonly service: CharacterService) { }
+
   @Query(() => [String])
   characters(): string[] {
-    return []
+    return this.service.getCharacters()
   }
 }
--- mhy/libs/character/feature-character/src/character/character.service.ts
+++ mhy/libs/character/feature-character/src/character/character.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { Span } from '@mhy/telemetry/tracing'
 
 @Injectable()
-export class CharacterService {}
+export class CharacterService {
+  @Span()
+  getCharacters(): string[] {
+    return []
+  }
+}
